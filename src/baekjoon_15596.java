b6615d7478bb3f960f87395bcca28f979e36f6d9
KimNamKwang <durante0916@gmail.com>
2022-12-15 11:28:03 +0900

    hfd

--- src/baekjoon_15596.java
+++ src/baekjoon_15596.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // 문제 파악
+// 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
 
 // 유추 파악
 
@@ -16,24 +17,13 @@ import java.io.InputStreamReader;
 
 // 실패 시 원인 기록
 public class baekjoon_15596 {
-    // public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    // int num;
+        int count = 0; // 입력받은 정수의 갯수 파악용
 
-    // try {
-    // num = Integer.parseInt(br.readLine().split(" "));
-    // System.out.println(num);
-    // } catch (NumberFormatException | IOException exception) {
-    // exception.printStackTrace();
-    // }
-
-    // }
-
-    public int sum(int num) {
-
-        return 0;
+        String input = br.readLine(); // 입력받은 값 문자열로 보관
+        String nums[] = input.split(" "); // 입력받은 정수들 배열에 보관
     }
-
 }
