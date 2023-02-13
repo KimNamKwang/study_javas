efea0dd9e49ea9999b140d82231dd9d4978618a9
KimNamKwang <durante0916@gmail.com>
2023-02-13 13:39:15 +0900

    handshakenexception -> https에서 http로 수정

--- src/ApiExplorersWithShelter.java
+++ src/ApiExplorersWithShelter.java
@@ -7,33 +7,30 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class ApiExplorersWithDatagokr {
+public class ApiExplorersWithShelter {
     public static void main(String[] args) throws UnsupportedEncodingException {
         /*
          * 외교부 국가 지역별 최신 안전소식(코로나 관련)
-         * https://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/
-         * getCountrySafetyNewsListNew?
+         * https://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List?
          * serviceKey=NzstB3xGw8fHulyfoEqU56BRhGdw45Jeq6BpoZJKuXa3rKcuyaGut5yVyhpua4lYU%
-         * 2F6lR5tF0KZdwOsTOEvvhw%3D%3D&
-         * returnType=JSON&
-         * numOfRows=10&
-         * pageNo=1&cond[country_nm::EQ]=중국
+         * 2F6lR5tF0KZdwOsTOEvvhw%3D%3D
+         * &pageNo=1
+         * &numOfRows=10
+         * &type=xml
          */
 
         StringBuilder urlBuilder = new StringBuilder(
-                "http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew"); /* URL */
+                "http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List"); /* URL */
         // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=").append(
                 "NzstB3xGw8fHulyfoEqU56BRhGdw45Jeq6BpoZJKuXa3rKcuyaGut5yVyhpua4lYU%2F6lR5tF0KZdwOsTOEvvhw%3D%3D");
         // Service Key
 
-        urlBuilder.append("&" + URLEncoder.encode("returnType", "UTF-8") + "="
-                + URLEncoder.encode("JSON", "UTF-8")); /* XML 또는 JSON */
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
-        // 한페이지 결과 수
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));// 페이지번호
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]", "UTF-8") + "="
-                + URLEncoder.encode("중국", "UTF-8")); /* ISO 2자리코드 */
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
+        urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "="
+                + URLEncoder.encode("xml", "UTF-8")); /* XML 또는 JSON */
+        // 한페이지 결과 수
 
         /* url로 값을 변환해줌 */
         URL url;
